d6b872a2b97fc4e541acc07c85e67a16f25942b7
root <root@iZu1rnh4d91Z.(none)>
2015-08-22 18:54:21 +0800

    add iftop

---
+++ install_iftop.sh
@@ -0,0 +1,5 @@
+yum install flex byacc  libpcap ncurses ncurses-devel
+
+wget ftp://fr2.rpmfind.net/linux/dag/redhat/el5/en/i386/dag/RPMS/iftop-0.17-1.el5.rf.i386.rpm
+
+rpm -ivh iftop-0.17-1.el5.rf.i386.rpm
